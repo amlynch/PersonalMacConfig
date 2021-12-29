4077f73d50b2cac7b9e8a5651fae0c6c7ec29ef5
Andrew L <3504573+amlynch@users.noreply.github.com>
2021-12-29 18:26:42 -0500

    Create YouTube_PlaylistImprovements.js

---
+++ YouTube_PlaylistImprovements.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         YouTube Playlist Improvements
+// @namespace    www.youtube.com
+// @version      0.1
+// @description  Add icons for easy playlist addition and removal
+// @author       Andrew Lynch
+// @match        http://www.youtube.com/*
+// @icon         none
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
